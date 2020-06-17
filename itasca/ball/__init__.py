f573b1e130e3ab09486353ba75ea84c79d5311b8
admin <admin@localhost>
2020-06-17 19:29:51 +0800

    对ball中的全局函数进行手工修复

--- itasca/ball/__init__.py
+++ itasca/ball/__init__.py
@@ -1,6 +1,7 @@
-from typing import Any, Tuple
+from typing import Any, Tuple, Iterator
 
 from . import thermal
+
 
 def _plist(*args, **kwargs) -> Any:
     """
@@ -9,35 +10,37 @@ def _plist(*args, **kwargs) -> Any:
     """
     pass
 
-def count(*args, **kwargs) -> Any:
+
+def count() -> int:
     """
-    () -> int.
     Get the number of balls.
     """
     pass
 
-def create(*args, **kwargs) -> Any:
+
+def create(*args, **kwargs) -> 'Ball':
     """
     (radius: double, centroid: vec [,id: int]) -> Ball object.
     Create a ball.
     """
     pass
 
-def energy(*args, **kwargs) -> Any:
+
+def energy(energy_name: str) -> float:
     """
-    (energy_name: str) -> float.
     Get the ball total energy contribution.
     """
     pass
 
-def find(*args, **kwargs) -> Any:
+
+def find(id: int) -> 'Ball':
     """
-    (id: int) -> Ball object.
     Get the ball object with the given ID number.
     """
     pass
 
-def inbox(*args, **kwargs) -> Any:
+
+def inbox(*args, **kwargs) -> Tuple['Ball']:
     """
     (lower_bound: vec, upper_bound: vec, intersect=True) -> Tuple of ball objects.
     Get a tuple of balls with extents intersecting a box.
@@ -46,21 +49,22 @@ def inbox(*args, **kwargs) -> Any:
     """
     pass
 
-def list(*args, **kwargs) -> Any:
+
+def list() -> Iterator['Ball']:
     """
-    () -> Ball iterator object.
     Get a ball iterator object.
     """
     pass
 
-def maxid(*args, **kwargs) -> Any:
+
+def maxid() -> int:
     """
-    () -> int.
     Get the maximum ball ID.
     """
     pass
 
-def near(*args, **kwargs) -> Any:
+
+def near(*args, **kwargs) -> 'Ball':
     """
     (point: vec, radius=0.0) -> Ball object.
     Find the closest ball to a point.
@@ -68,16 +72,15 @@ def near(*args, **kwargs) -> Any:
     """
     pass
 
+
 class Ball:
-    __hash__: Any = ...
-    @classmethod
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self) -> None:
         """
         Create and return a new object.
          See help(type) for accurate signature.
         """
         pass
-    
+
     def contact_count(self, *args, **kwargs) -> Any:
         """
         (all=False, type=None) -> int.
@@ -87,7 +90,7 @@ class Ball:
         If the optional keyword argument all is True the count includes virtual contacts.
         """
         pass
-    
+
     def contacts(self, *args, **kwargs) -> Any:
         """
         ([piece: ball], all=False, type=None) -> tuple of Contact objects.
@@ -98,56 +101,53 @@ class Ball:
         If the (optional) keyword argument all is True the returned list includes virtual contacts.
         """
         pass
-    
-    def damp(self, *args, **kwargs) -> Any:
+
+    def damp(self) -> float:
         """
-        () -> float.
         Get the ball local damping.
         """
         pass
-    
-    def delete(self, *args, **kwargs) -> Any:
+
+    def delete(self) -> None:
         """
-        () -> None.
         Delete this ball.
         """
         pass
-    
-    def density(self, *args, **kwargs) -> Any:
+
+    def density(self) -> float:
         """
-        () -> float.
         Get the ball density.
         """
         pass
-    
+
     def disp(self, *args, **kwargs) -> Any:
         """
         () -> vec.
         Get the ball displacement (vector).
         """
         pass
-    
+
     def disp_x(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the x-component of the ball displacement.
         """
         pass
-    
+
     def disp_y(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the y-component of the ball displacement.
         """
         pass
-    
+
     def extra(self, *args, **kwargs) -> Any:
         """
         (slot: int) -> any.
         Get the ball extra data in the given slot.
         """
         pass
-    
+
     def fix(self, *args, **kwargs) -> Any:
         """
         (component: int) -> bool.
@@ -155,84 +155,84 @@ class Ball:
         The first argument is the vector component.
         """
         pass
-    
+
     def force_app(self, *args, **kwargs) -> Any:
         """
         () -> vec.
         Get the ball applied force (vector).
         """
         pass
-    
+
     def force_app_x(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the x-component of the ball applied force.
         """
         pass
-    
+
     def force_app_y(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the y-component of the ball applied force.
         """
         pass
-    
+
     def force_contact(self, *args, **kwargs) -> Any:
         """
         () -> vec.
         Get the ball contact force (vector).
         """
         pass
-    
+
     def force_contact_x(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the x-component of the ball contact force.
         """
         pass
-    
+
     def force_contact_y(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the y-component of the ball contact force.
         """
         pass
-    
+
     def force_unbal(self, *args, **kwargs) -> Any:
         """
         () -> vec.
         Get the ball unbalanced force (vector).
         """
         pass
-    
+
     def force_unbal_x(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the x-component of the ball unbalanced force.
         """
         pass
-    
+
     def force_unbal_y(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the y-component of the ball unbalanced force.
         """
         pass
-    
+
     def fragment(self, *args, **kwargs) -> Any:
         """
         () -> int.
         Get the ball fragment ID.
         """
         pass
-    
+
     def group(self, *args, **kwargs) -> Any:
         """
         ([slot: str]) -> str.
         Get the ball group name in a given slot.
         """
         pass
-    
+
     def group_remove(self, *args, **kwargs) -> Any:
         """
         (group_name: str) -> bool.
@@ -241,7 +241,7 @@ class Ball:
         The return value is a bool which is True if the group was removed from any slot, otherwise False.
         """
         pass
-    
+
     def groups(self, *args, **kwargs) -> Any:
         """
         () -> {slot: group_name}.
@@ -249,21 +249,21 @@ class Ball:
         The keys of the dictionary are the slot names and the values are the group names.
         """
         pass
-    
+
     def has_prop(self, *args, **kwargs) -> Any:
         """
         (property_name: str) -> bool.
         Returns True if the ball has the given surface property.
         """
         pass
-    
+
     def id(self, *args, **kwargs) -> Any:
         """
         () -> int.
         Get the ball id.
         """
         pass
-    
+
     def in_group(self, *args, **kwargs) -> Any:
         """
         (group_name: str[, slot: str]) -> bool.
@@ -272,147 +272,147 @@ class Ball:
         Otherwise, all group slots are searched.
         """
         pass
-    
+
     def mass(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the ball inertial mass.
         """
         pass
-    
+
     def mass_real(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the ball real (gravitational) mass.
         """
         pass
-    
+
     def moi(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the ball moment of inertia.
         """
         pass
-    
+
     def moi_real(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the ball real (gravitational) moment of inertia.
         """
         pass
-    
+
     def moment_app(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the ball applied moment.
         """
         pass
-    
+
     def moment_contact(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the ball contact moment.
         """
         pass
-    
+
     def moment_unbal(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the ball unbalanced moment.
         """
         pass
-    
+
     def pos(self, *args, **kwargs) -> Any:
         """
         () -> vec.
         Get the ball centroid location (vector).
         """
         pass
-    
+
     def pos_x(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the x-component of the ball centroid location.
         """
         pass
-    
+
     def pos_y(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the y-component of the ball centroid location.
         """
         pass
-    
+
     def prop(self, *args, **kwargs) -> Any:
         """
         (property_name: str) -> any.
         Get a surface property value of this ball.
         """
         pass
-    
+
     def props(self, *args, **kwargs) -> Any:
         """
         () -> dict {str: any}.
         Get a dictionary of all the surface properties of this ball.
         """
         pass
-    
+
     def radius(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the ball radius.
         """
         pass
-    
+
     def rotation(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the ball orientation.
         """
         pass
-    
+
     def set_damp(self, *args, **kwargs) -> Any:
         """
         (value: float) -> None.
         Set the ball local damping.
         """
         pass
-    
+
     def set_density(self, *args, **kwargs) -> Any:
         """
         (value: float) -> None.
         Set the ball density.
         """
         pass
-    
+
     def set_disp(self, *args, **kwargs) -> Any:
         """
         (value: vec) -> None.
         Set the ball displacement (vector).
         """
         pass
-    
+
     def set_disp_x(self, *args, **kwargs) -> Any:
         """
         (value: float) -> None.
         Set the x-component of the ball displacement.
         """
         pass
-    
+
     def set_disp_y(self, *args, **kwargs) -> Any:
         """
         (value: float) -> None.
         Set the y-component of the ball displacement.
         """
         pass
-    
+
     def set_extra(self, *args, **kwargs) -> Any:
         """
         (slot: int, value: any) -> None.
         Set the ball extra data in the given slot.
         """
         pass
-    
+
     def set_fix(self, *args, **kwargs) -> Any:
         """
         (component: int, fix: bool) -> None.
@@ -420,225 +420,225 @@ class Ball:
         The first argument is the vector component to set and the second components is a the fixity flag.
         """
         pass
-    
+
     def set_force_app(self, *args, **kwargs) -> Any:
         """
         (value: vec) -> None.
         Set the ball applied force (vector).
         """
         pass
-    
+
     def set_force_app_x(self, *args, **kwargs) -> Any:
         """
         (value: float) -> None.
         Set the x-component of the ball applied force.
         """
         pass
-    
+
     def set_force_app_y(self, *args, **kwargs) -> Any:
         """
         (value: float) -> None.
         Set the y-component of the ball applied force.
         """
         pass
-    
+
     def set_force_contact(self, *args, **kwargs) -> Any:
         """
         (value: vec) -> None.
         Set the ball contact force (vector).
         """
         pass
-    
+
     def set_force_contact_x(self, *args, **kwargs) -> Any:
         """
         (value: float) -> None.
         Set the x-component of the ball contact force.
         """
         pass
-    
+
     def set_force_contact_y(self, *args, **kwargs) -> Any:
         """
         (value: float) -> None.
         Set the y-component of the ball contact force.
         """
         pass
-    
+
     def set_fragment(self, *args, **kwargs) -> Any:
         """
         (id: int) -> None.
         Set ball fragment ID.
         """
         pass
-    
+
     def set_group(self, *args, **kwargs) -> Any:
         """
         (group_name: str[, slot: str]) -> None.
         Set the ball group name in a given slot.
         """
         pass
-    
+
     def set_moment_app(self, *args, **kwargs) -> Any:
         """
         (value: float) -> None.
         Set the ball applied moment.
         """
         pass
-    
+
     def set_moment_contact(self, *args, **kwargs) -> Any:
         """
         (value: float) -> None.
         Set the ball contact moment.
         """
         pass
-    
+
     def set_pos(self, *args, **kwargs) -> Any:
         """
         (value: vec) -> None.
         Set the ball centroid location (vector).
         """
         pass
-    
+
     def set_pos_x(self, *args, **kwargs) -> Any:
         """
         (value: float) -> None.
         Set the x-component of the ball centroid location.
         """
         pass
-    
+
     def set_pos_y(self, *args, **kwargs) -> Any:
         """
         (value: float) -> None.
         Set the y-component of the ball centroid location.
         """
         pass
-    
+
     def set_prop(self, *args, **kwargs) -> Any:
         """
         (property_name: str, value: any) -> None.
         Set a surface property of this ball.
         """
         pass
-    
+
     def set_radius(self, *args, **kwargs) -> Any:
         """
         (value: float) -> None.
         Set the ball radius.
         """
         pass
-    
+
     def set_rotation(self, *args, **kwargs) -> Any:
         """
         (value: float) -> None.
         Set the ball orientation.
         """
         pass
-    
+
     def set_spin(self, *args, **kwargs) -> Any:
         """
         (value: float) -> None.
         Set the ball angular velocity.
         """
         pass
-    
+
     def set_vel(self, *args, **kwargs) -> Any:
         """
         (value: vec) -> None.
         Set the ball velocity (vector).
         """
         pass
-    
+
     def set_vel_x(self, *args, **kwargs) -> Any:
         """
         (value: float) -> None.
         Set the x-component of the ball velocity.
         """
         pass
-    
+
     def set_vel_y(self, *args, **kwargs) -> Any:
         """
         (value: float) -> None.
         Set the y-component of the ball velocity.
         """
         pass
-    
+
     def spin(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the ball angular velocity.
         """
         pass
-    
+
     def stress(self, *args, **kwargs) -> Any:
         """
         () -> tensor.
         Get the stress tensor arising from all contacts acting on the ball.
         """
         pass
-    
+
     def valid(self, *args, **kwargs) -> Any:
         """
         () -> bool.
         Returns True if this ball is live.
         """
         pass
-    
+
     def vel(self, *args, **kwargs) -> Any:
         """
         () -> vec.
         Get the ball velocity (vector).
         """
         pass
-    
+
     def vel_x(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the x-component of the ball velocity.
         """
         pass
-    
+
     def vel_y(self, *args, **kwargs) -> Any:
         """
         () -> float.
         Get the y-component of the ball velocity.
         """
         pass
-    
+
     def __eq__(self, other) -> Any:
         """
         Return self==value.
         """
         pass
-    
+
     def __ge__(self, other) -> Any:
         """
         Return self>=value.
         """
         pass
-    
+
     def __gt__(self, other) -> Any:
         """
         Return self>value.
         """
         pass
-    
+
     def __le__(self, other) -> Any:
         """
         Return self<=value.
         """
         pass
-    
+
     def __lt__(self, other) -> Any:
         """
         Return self<value.
         """
         pass
-    
+
     def __ne__(self, other) -> Any:
         """
         Return self!=value.
         """
         pass
-    
+
 
 class BallIter:
     @classmethod
@@ -648,16 +648,15 @@ class BallIter:
          See help(type) for accurate signature.
         """
         pass
-    
+
     def __iter__(self) -> Any:
         """
         Implement iter(self).
         """
         pass
-    
+
     def __next__(self) -> Any:
         """
         Implement next(self).
         """
         pass
-    
