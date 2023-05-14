df74a44100b7ce740392241eab3e148ffbe751e8
panhaoyu <panhaoyu.china@outlook.com>
2023-05-14 09:56:20 +0800

    Change python version before upload

--- build.py
+++ build.py
@@ -2,5 +2,9 @@ import toml
 
 with open('pyproject.toml', 'r', encoding='utf-8') as f:
     data = toml.load(f)
-print(data)
+dependencies = data['tool']['poetry']['dependencies']
+previous = dependencies['python']
+# dependencies['python'] = '^3.6'
+with open('pyproject.toml', 'w', encoding='utf-8') as f:
+    toml.dump(data, f)
 # os.system('phy-upload -r pypi')
