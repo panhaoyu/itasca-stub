5b35c36241fde46b5a472f46870f0be69e3b2d66
panhaoyu <panhaoyu.china@outlook.com>
2024-07-24 16:32:06 +0800

    add the custom stubgenc.py back

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import inspect
-import inspect
+import importlib
 import json
 import os
 import os.path
@@ -11,77 +10,18 @@ import traceback
 import types
 from pathlib import Path
 from typing import Dict, Optional, Set
-from typing import List
 
 import _itasca_com
 import numpy
 import select
-from mypy.moduleinspect import is_c_module
-from mypy.stubgenc import strip_or_import, get_type_fullname, add_typing_import, generate_c_type_stub, is_c_type, \
-    generate_c_function_stub, is_c_function
 
 import itasca
 
 itasca.command(r'program directory custom "F:/projects/itasca-stub"')
 
+import stubgenc
 
-def generate_stub_for_c_module(module: types.ModuleType,
-                               target: Path,
-                               sigs: Optional[Dict[str, str]] = None,
-                               class_sigs: Optional[Dict[str, str]] = None) -> None:
-    """Generate stub for C module.
-
-    This combines simple runtime introspection (looking for docstrings and attributes
-    with simple builtin types) and signatures inferred from .rst documentation (if given).
-
-    If directory for target doesn't exist it will be created. Existing stub
-    will be overwritten.
-    """
-    target = str(target)
-    assert is_c_module(module), f'{module} is not a C module'
-    subdir = os.path.dirname(target)
-    if subdir and not os.path.isdir(subdir):
-        os.makedirs(subdir)
-    imports: List[str] = []
-    functions: List[str] = []
-    done = set()
-    items = sorted(module.__dict__.items(), key=lambda x: x[0])
-    for name, obj in items:
-        if is_c_function(obj):
-            generate_c_function_stub(module, name, obj, functions, imports=imports, sigs=sigs)
-            done.add(name)
-    types: List[str] = []
-    for name, obj in items:
-        if name.startswith('__') and name.endswith('__'):
-            continue
-        if is_c_type(obj):
-            generate_c_type_stub(module, name, obj, types, imports=imports, sigs=sigs,
-                                 class_sigs=class_sigs)
-            done.add(name)
-    variables = []
-    for name, obj in items:
-        if name.startswith('__') and name.endswith('__'):
-            continue
-        if name not in done and not inspect.ismodule(obj):
-            type_str = strip_or_import(get_type_fullname(type(obj)), module, imports)
-            variables.append(f'{name}: {type_str}')
-    output = []
-    for line in sorted(set(imports)):
-        output.append(line)
-    for line in variables:
-        output.append(line)
-    for line in types:
-        if line.startswith('class') and output and output[-1]:
-            output.append('')
-        output.append(line)
-    if output and functions:
-        output.append('')
-    for line in functions:
-        output.append(line)
-    output = add_typing_import(output)
-    with open(target, 'w') as file:
-        for line in output:
-            file.write(f'{line}\n')
+importlib.reload(stubgenc)
 
 
 class PackageGenerator:
@@ -118,7 +58,7 @@ class PackageGenerator:
         for name, module in self.all_packages.items():
             path = Path(name.replace('.', '/')) / '__init__.py'
             try:
-                generate_stub_for_c_module(module, path)
+                stubgenc.generate_stub_for_c_module(module, str(path))
             except (AssertionError, TypeError):
                 traceback.print_exc()
                 print('Error: ', name)
