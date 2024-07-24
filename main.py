7a3f57f1873b0aa9f59399b0a21a878205210a15
panhaoyu <panhaoyu.china@outlook.com>
2024-07-24 15:45:22 +0800

    remove previous stubgenc

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import importlib
 import types
+from typing import Dict
+
+import mypy.stubgen
 
 import itasca
 
 itasca.command(r'program directory custom "F:/projects/itasca-stub"')
-import stubgenc
 
-importlib.reload(stubgenc)
+importlib.reload(mypy.stubgen)
 
 
-class PackageGenerator(object):
+class PackageGenerator:
     def __init__(self, package):
         self.package = package
-        self.all_packages = dict()
+        self.all_packages: Dict[str, types.ModuleType] = {}
 
-    def find_package(self, name, module):
+    def find_package(self, name: str, module: types.ModuleType) -> None:
         for sub_name, instance in vars(module).items():
             if not isinstance(instance, types.ModuleType):
                 continue
@@ -31,18 +33,17 @@ class PackageGenerator(object):
             self.all_packages[total_name] = instance
             self.find_package(total_name, instance)
 
-    def generate(self):
+    def generate(self) -> None:
         self.all_packages['itasca'] = self.package
         self.find_package('itasca', self.package)
         for name, package in self.all_packages.items():
-            path = '{}/__init__.py'.format(name)
-            other_imports = [key.replace(name + '/', '')
+            path = f'{name}/__init__.py'
+            other_imports = [key.replace(f'{name}/', '')
                              for key in self.all_packages.keys()
-                             if key.startswith(name + '/')]
-            other_imports = [other_import for other_import in other_imports if other_import]
-            other_imports = [other_import for other_import in other_imports if '/' not in other_import]
+                             if key.startswith(f'{name}/')]
+            other_imports = [other_import for other_import in other_imports if other_import and '/' not in other_import]
             try:
-                stubgenc.generate_stub_for_c_module(package, path, other_import=other_imports)
+                mypy.stubgen.generate_stub_for_c_module(package, path)
             except (AssertionError, TypeError):
                 print('Error', name)
 
